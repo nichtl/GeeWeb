ea87b1207c696138ed0e9083257cba351866a54e
xujian8 <xujian8@tuniu.com>
2024-11-04 20:09:47 +0800

    AMID-24-11-04  init

---
+++ go.mod
@@ -0,0 +1,4 @@
+module GeeWeb
+
+go 1.22
+require gee v0.0.0
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"gee"
+	"net/http"
+)
+
+//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
+
+// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
+
+func main() {
+	r := gee.New()
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *gee.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
+
+		v1.GET("/hello", func(c *gee.Context) {
+			// expect /hello?name=geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *gee.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *gee.Context) {
+			c.JSON(http.StatusOK, gee.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+
+	}
+	r.Run(":9999")
+}
+
+//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
+// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
